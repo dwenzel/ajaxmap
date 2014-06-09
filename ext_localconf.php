f8139cc577225e92d66e7fcfc336df190599d761
Dirk Wenzel <der-wenzel@gmx.de>
2014-06-09 15:13:21 +0200

    include of TypoScript for JavaScript depending on Extension Manager settings

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,21 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+if (!empty($settings['includeJQuery'])) {
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:placements/Resources/Private/TypoScript/jQuery.ts>');
+}
+
+if (!empty($settings['includeGoogleMaps'])) {
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'. $_EXTKEY . '/Resources/Private/TypoScript/googleMaps.ts>');
+}
+if (!empty($settings['includeJavaScript'])) {
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'. $_EXTKEY . '/Resources/Private/TypoScript/javascript.ts>');
+}
+
 Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
 	'Map',
