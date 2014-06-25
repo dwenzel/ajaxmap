7b6610296a90cbee88ecae4eb1f3cf8c9ec98409
Dirk Wenzel <der-wenzel@gmx.de>
2014-06-25 22:35:54 +0200

    parent class changed to Tx_Ajaxmap_DomainObject_AbstractEntity

--- Classes/Domain/Model/Address.php
+++ Classes/Domain/Model/Address.php
@@ -31,7 +31,7 @@
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class Tx_Ajaxmap_Domain_Model_Address extends Tx_Extbase_DomainObject_AbstractEntity {
+class Tx_Ajaxmap_Domain_Model_Address extends Tx_Ajaxmap_DomainObject_AbstractEntity {
 
 }
 ?>
