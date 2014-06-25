6933ba1b107a8fb5c64fad68447694332c49c227
Dirk Wenzel <der-wenzel@gmx.de>
2014-06-25 18:04:14 +0200

    parent class changed

--- Classes/Domain/Model/Place.php
+++ Classes/Domain/Model/Place.php
@@ -31,7 +31,7 @@
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class Tx_Ajaxmap_Domain_Model_Place extends Tx_Extbase_DomainObject_AbstractEntity {
+class Tx_Ajaxmap_Domain_Model_Place extends Tx_Ajaxmap_DomainObject_AbstractEntity {
 
 	/**
 	 * Geographic location - defined by its latitude and longitude
