3b87246853a5e39a2e7b533e216b426f451be838
Dirk Wenzel <der-wenzel@gmx.de>
2014-06-27 14:20:32 +0200

    method map implemented

--- Classes/Utility/MappingUtility.php
+++ Classes/Utility/MappingUtility.php
@@ -32,7 +32,7 @@
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class Tx_Ajaxmap_Utilitiy_MappingUtility {
+class Tx_Ajaxmap_Utility_MappingUtility {
 
 	/**
 	 * Return a new array mapped according settings
@@ -41,7 +41,20 @@ class Tx_Ajaxmap_Utilitiy_MappingUtility {
 	 * @var array $settings Array with mapping settings
 	 * @return array
 	 */
-		public function map($source, settings) {
-
+		public function map($source, $settings) {
+			$result = $source;
+			$mappedArray = array();
+			$models = array_keys($source);
+			foreach($source as $key=>$value) {
+				if(array_key_exists($key, $settings)) {
+					$mappedArray[$settings[$key]] = $value;
+				} else {
+					$mappedArray[$key] = $value;
+				}
+			}
+			if(count($mappedArray)) {
+				$result = $mappedArray;
+			}
+			return $result;
 		}
 } 
