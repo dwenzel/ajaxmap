60262f7dc00a35d18980cff60971c01e38d0a335
Emoeke Horvath <e.horvath@familie-redlich.de>
2019-11-14 11:29:22 +0100

    [Task] deactivate fortawesome (alias fontawesome)

--- FE-development/src/assets/scripts/main.js
+++ FE-development/src/assets/scripts/main.js
@@ -4,7 +4,7 @@ import ajaxMap from './lib/ajaxMap';
 import '../../../dist/css/main.css'
 //import {css} from 'jquery.fancytree/dist/skin-awesome/ui.fancytree.min.css';
 import 'jquery.fancytree/dist/skin-lion/ui.fancytree.css';
-import {library, dom} from '@fortawesome/fontawesome-svg-core'
+/*import {library, dom} from '@fortawesome/fontawesome-svg-core'
 
 // import folder icons for tree
 import {
@@ -40,20 +40,19 @@ library.add(
     farSquare
 );
 
+*/
+
 $(document).ready(function() {
 
     if (!mapStore || !(mapStore instanceof Array)) {
-        console.log(' no mapstore', mapStore)
+        console.log(' no mapstore', mapStore);
         return;
     }
 
     const configData = {
         mapSettings: window.mapSettings,
         mapStore: window.mapStore
-    }
+    };
 
-    ajaxMap.init(configData);//initialize all maps
-})
-
-
-
+    ajaxMap.init(configData); //initialize all maps
+});
