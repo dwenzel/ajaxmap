e91b2dfc142edb4b0f9e0e6b27c65a54b6456192
Emoeke Horvath <e.horvath@familie-redlich.de>
2019-11-14 11:39:34 +0100

    [Task] #14201 Update html5/styles setup

---
+++ FE-development/src/patterns/components/map-elements/option-filter-select.config.js
@@ -0,0 +1,44 @@
+/*
+ * _______________
+ * |       .-.   |
+ * |      // ``  |
+ * |     //      |
+ * |  == ===-_.-'|
+ * |   //  //    |
+ * |__//_________|
+ *
+ * Copyright (c) ${YEAR} familie-redlich :systeme <systeme@familie-redlich.de>
+ * @link     http://www.familie-redlich.de
+ *
+ *
+ */
+
+/**
+ * Created by d.eggermann on 07.11.19.
+ */
+
+
+const labels = [
+    'Option 1',
+    'Option 2',
+    'Option 3'
+];
+
+const values = [
+    'Option 1',
+    'Option 2',
+    'Option 3'
+];
+
+module.exports = {
+
+    context: {
+        label: 'Filter name',
+        options: labels.map((label, i) => {
+            return {
+                label: label,
+                value: values[i]
+            }
+        })
+    }
+};
