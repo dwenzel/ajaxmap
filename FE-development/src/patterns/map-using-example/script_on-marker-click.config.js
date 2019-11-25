393c19ea1df9a3c31cb665a25756907c7b25e88f
Dominik Eggermann <d.eggermann@familie-redlich.de>
2019-11-25 21:39:17 +0100

    [TASK] add basic scroll list item in places- tree

--- FE-development/src/patterns/map-using-example/script_on-marker-click.config.js
+++ FE-development/src/patterns/map-using-example/script_on-marker-click.config.js
@@ -5,56 +5,14 @@ const onMarkerClickTemplate = function(mapEntry, place, $) {
 
     treeNode.setSelected(true)
 
-    var ulEl = treeNode.parent.ul;
-    var liEl = treeNode.li;
+    //    var ulEl = treeNode.parent.ul;
+    //  var liEl = treeNode.li;
 
-    var $li = $(liEl);
-    var $ul = $(ulEl);
-
-    function scrollUL(li) {
-        // scroll UL to make li visible
-        // li can be the li element or its id
-        if (typeof li !== "object") {
-            li = document.getElementById(li);
-        }
-        var ul = li.parentNode;
-        // fudge adjustment for borders effect on offsetHeight
-        var fudge = 4;
-        // bottom most position needed for viewing
-        var bottom = (ul.scrollTop + (ul.offsetHeight - fudge) - li.offsetHeight);
-        // top most position needed for viewing
-        var top = ul.scrollTop + fudge;
-        if (li.offsetTop <= top) {
-            // move to top position if LI above it
-            // use algebra to subtract fudge from both sides to solve for ul.scrollTop
-            ul.scrollTop = li.offsetTop - fudge;
-        } else if (li.offsetTop >= bottom) {
-            // move to bottom position if LI below it
-            // use algebra to subtract ((ul.offsetHeight - fudge) - li.offsetHeight) from both sides to solve for ul.scrollTop
-            ul.scrollTop = li.offsetTop - ((ul.offsetHeight - fudge) - li.offsetHeight);
-        }
-    };
-
-    scrollUL(liEl);
-
-    return;
-
-
-    liEl.scrollIntoView()
-    /*
-     var offset = $firstLi.position().top;
-     var ulScrollTop= $li.position().top - offset;
-
-
-
-     $ul.animate({
-     scrollTop: $li.position().top
-     }, 'slow');
-     //   $ul.scrollTop(ulScrollTop);
-
-     //$ul[0].style.display='none'
-     console.log(ulEl,'****', ulScrollTop)
-     */
+    treeNode.li.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    });
 }.toString();
 
 const onMarkerClick = `function (){
