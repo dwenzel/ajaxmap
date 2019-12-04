92fc8d5d716eb8e3817e55b95cf1dc87fe601d83
Elias Häußler <e.haeussler@familie-redlich.de>
2019-12-04 10:32:23 +0100

    [TASK] Reformat ext_localconf and avoid error-prone use statements

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,39 +1,33 @@
 <?php
+defined('TYPO3_MODE') or die();
 
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use DWenzel\Ajaxmap\Configuration\SettingsInterface as SI;
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
-$boot = function () {
-
-    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][SI::CACHE_CHILDREN])) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][SI::CACHE_CHILDREN] = [];
+(function ($extensionKey) {
+    // Add cache configurations
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\DWenzel\Ajaxmap\Configuration\SettingsInterface::CACHE_CHILDREN])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\DWenzel\Ajaxmap\Configuration\SettingsInterface::CACHE_CHILDREN] = [];
     }
-    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][SI::CACHE_AJAX_DATA])) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][SI::CACHE_AJAX_DATA] = [];
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\DWenzel\Ajaxmap\Configuration\SettingsInterface::CACHE_AJAX_DATA])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\DWenzel\Ajaxmap\Configuration\SettingsInterface::CACHE_AJAX_DATA] = [];
     }
 
-    $settings = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('ajaxmap');
-
-    if (!empty($settings['includeJavaScript'])) {
+    // Include global JavaScript
+    $settings = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get($extensionKey);
+    if ($settings['includeJavaScript'] ?? false) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ajaxmap/Resources/Private/TypoScript/javaScript.typoScript">');
+            '@import "EXT:' . $extensionKey . '/Resources/Private/TypoScript/javaScript.typoScript"'
+        );
     }
+
+    // Configure plugins
     TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'DWenzel.' . 'ajaxmap',
+        'DWenzel.' . $extensionKey,
         'Map',
         [
-            'Place' => 'show, ajaxShow',
+            'Place' => 'show,ajaxShow',
             'Map' => 'show,search',
         ],
         [
             'Map' => 'show,search'
         ]
     );
-};
-
-$boot();
-unset($boot);
+})('ajaxmap');
