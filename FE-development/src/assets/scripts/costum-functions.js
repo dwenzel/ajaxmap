f6601aabe10677953fbff3f2ebb0074fb11085dc
Emoeke Horvath <e.horvath@familie-redlich.de>
2019-12-10 16:50:37 +0100

    [DEBUG] #14378 map jumps, if user klick marker

--- FE-development/src/assets/scripts/costum-functions.js
+++ FE-development/src/assets/scripts/costum-functions.js
@@ -1,23 +1,14 @@
 import $ from 'jquery';
 
 const customMapFunctions = {
-    /*onMarkerClick: function(mapEntry, place) {
-
-        const treeNode = place.placeInstance.treeNode;//has all domNodes
-        treeNode.setSelected(true);
-
-        treeNode.li.scrollIntoView({// caniuse :78% /use polyfill : https://github.com/iamdustan/smoothscroll
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest'
-        });
-    },*/
     onMarkerClick: function(mapEntry, place) {
 
-        const treeNode = place.placeInstance.treeNode;//has all domNodes
-        treeNode.setSelected(true);
+        const treeNode = place.placeInstance.treeNode; // has all domNodes
+        let topPosition = $(treeNode.li).position().top,
+            listWrapper = $(treeNode.li).closest('.am__sb__scroll-wrapper');
 
-        console.log('treeNode.setSelected(true)', treeNode.setSelected(true));
+        treeNode.setSelected(true);
+        $(listWrapper).animate({scrollTop: topPosition}, 600);
     },
     placeSortFunction: function(a, b) {
         let valA = a.data.distance;
